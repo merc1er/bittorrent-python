dbdab0cbb3046a2628c3c2b1ec5343f4ad5b6f3b
merc1er <corentin@mercier.link>
2025-01-08 15:23:08 +0700

    Add info command

--- app/main.py
+++ app/main.py
@@ -6,6 +6,33 @@ import bencodepy
 bc = bencodepy.BencodeDecoder(encoding="utf-8")
 
 
+def read_torrent_file(file_path: str) -> str:
+    def convert_bytes_to_str(obj):
+        # Recursively convert all byte strings to normal strings
+        if isinstance(obj, dict):
+            result = {}
+            for key, value in obj.items():
+                result[convert_bytes_to_str(key)] = convert_bytes_to_str(value)
+            return result
+        elif isinstance(obj, list):
+            result = []
+            for item in obj:
+                result.append(convert_bytes_to_str(item))
+            return result
+        elif isinstance(obj, bytes):
+            return obj.decode("utf-8", errors="replace")  # Decode bytes to string
+        else:
+            return obj
+
+    with open(file_path, "rb") as f:
+        data = f.read()
+        decoded_data = bencodepy.decode(data)
+        stringified_data = convert_bytes_to_str(decoded_data)
+
+    print(stringified_data)
+    return stringified_data
+
+
 def main():
     command = sys.argv[1]
 
@@ -13,6 +40,10 @@ def main():
         bencoded_value = sys.argv[2].encode()
         decoded_value = bc.decode(bencoded_value)
         print(json.dumps(decoded_value))
+    elif command == "info":
+        decoded_value = read_torrent_file(sys.argv[2])
+        print("Tracker URL:", decoded_value["announce"])
+        print("Length:", decoded_value["info"]["length"])
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
---
+++ tests.sh
@@ -0,0 +1,7 @@
+python app/main.py decode i52e
+python app/main.py decode 6:orange
+python app/main.py decode lli877e6:bananaee
+python app/main.py decode d3:foo3:bar5:helloi52ee
+python app/main.py decode d3:foo9:blueberry5:helloi52ee
+
+python app/main.py info sample.torrent
