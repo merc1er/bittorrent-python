16ed2ef3ad8d96a145195ca0024597e74a1888c1
merc1er <corentin@mercier.link>
2025-01-13 00:43:41 +0700

    Decode peers

--- app/main.py
+++ app/main.py
@@ -30,12 +30,23 @@ def decode_pieces(pieces: bytes):
         pieces = pieces[20:]
 
 
+def decode_peers(peers: bytes) -> list[str]:
+    decoded_peers = []
+    while peers:
+        ip = ".".join(str(x) for x in peers[:4])
+        port = int.from_bytes(peers[4:6], byteorder="big")
+        decoded_peers.append(f"{ip}:{port}")
+        peers = peers[6:]
+
+    return decoded_peers
+
+
 def get_peers(url: str, info_hash: dict, left: int) -> str:
-    url_encoded_info_hash = bencodepy.encode(info_hash)
+    url_encoded_info_hash = sha1(bencodepy.encode(info_hash)).digest()
 
     params = {
         "info_hash": url_encoded_info_hash,
-        "peer_id": "superpeer",
+        "peer_id": "super_duper_peer____",
         "port": 6881,
         "uploaded": 0,
         "downloaded": 0,
@@ -49,8 +60,8 @@ def get_peers(url: str, info_hash: dict, left: int) -> str:
             print(response.text)
         print(f"Error: {e}")
 
-    decoded_response = bc.decode(response.content)
-    return decoded_response
+    decoded_response = bencodepy.decode(response.content)
+    return decode_peers(decoded_response[b"peers"])
 
 
 def main():
@@ -60,24 +71,26 @@ def main():
         bencoded_value = sys.argv[2].encode()
         decoded_value = bc.decode(bencoded_value)
         print(json.dumps(decoded_value))
-    elif command == "info":
+    elif command == "info" or command == "peers":
         decoded_value = read_torrent_file_raw(sys.argv[2])
         info_hash = calculate_sha1(decoded_value[b"info"])
         tracker_url = decoded_value[b"announce"].decode("utf-8")
 
-        print("Tracker URL:", tracker_url)
-        print("Length:", decoded_value[b"info"][b"length"])
-        print("Info Hash:", info_hash)
-        print("Piece Length:", decoded_value[b"info"][b"piece length"])
-        print("Piece Hashes:")
-        decode_pieces(decoded_value[b"info"][b"pieces"])
-
-        peers = get_peers(
-            url=tracker_url,
-            info_hash=decoded_value[b"info"],
-            left=decoded_value[b"info"][b"length"],
-        )
-        print(f"Peers: {peers}")
+        if command == "info":
+            print("Tracker URL:", tracker_url)
+            print("Length:", decoded_value[b"info"][b"length"])
+            print("Info Hash:", info_hash)
+            print("Piece Length:", decoded_value[b"info"][b"piece length"])
+            print("Piece Hashes:")
+            decode_pieces(decoded_value[b"info"][b"pieces"])
+        elif command == "peers":
+            peers = get_peers(
+                url=tracker_url,
+                info_hash=decoded_value[b"info"],
+                left=decoded_value[b"info"][b"length"],
+            )
+            for peer in peers:
+                print(peer)
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
