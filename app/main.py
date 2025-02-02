7598981ac67238b5c232a83061f49be4b207e310
merc1er <corentin@mercier.link>
2025-02-02 17:29:37 +0700

    Use match-case

--- app/main.py
+++ app/main.py
@@ -21,61 +21,64 @@ def read_torrent_file_raw(file_path: str) -> dict:
 def main():
     command = sys.argv[1]
 
-    if command == "decode":
-        bencoded_value = sys.argv[2].encode()
-        decoded_value = bc.decode(bencoded_value)
-        print(json.dumps(decoded_value))
-    elif command in ["info", "peers", "handshake"]:
-        torrent = Torrent.from_file(sys.argv[2])
+    match command:
+        case "decode":
+            bencoded_value = sys.argv[2].encode()
+            decoded_value = bc.decode(bencoded_value)
+            print(json.dumps(decoded_value))
+        case "info" | "peers" | "handshake":
+            torrent = Torrent.from_file(sys.argv[2])
 
-        if command == "info":
-            torrent.print_info()
-        elif command == "peers":
-            peers = get_peers(
-                url=torrent.tracker_url,
-                info=torrent.info,
-                left=torrent.length,
-            )
-            for peer in peers:
-                print(peer)
-        elif command == "handshake":
-            ip, port = sys.argv[3].split(":")
-            # print(f"Connecting to {ip}:{port}")
-            perform_handshake_standalone(ip, port, torrent.info_hash)
-    elif command == "download_piece":
-        output_file_path = sys.argv[3]
-        torrent = Torrent.from_file(sys.argv[4])
-        total_number_of_pieces = len(torrent.pieces)
-        print(f"Total number of pieces: {total_number_of_pieces}")
-        torrent_file_content = read_torrent_file_raw(sys.argv[4])
-        piece_index = int(sys.argv[5])
-        download_piece(
-            torrent_file_content, piece_index, output_file_path, total_number_of_pieces
-        )
-    elif command == "download":
-        output_file_path = sys.argv[3]
-        torrent = Torrent.from_file(sys.argv[4])
-        total_number_of_pieces = len(torrent.pieces)
-        print(f"Total number of pieces: {total_number_of_pieces}")
-        torrent_file_content = read_torrent_file_raw(sys.argv[4])
-
-        for piece_index in range(total_number_of_pieces):
+            if command == "info":
+                torrent.print_info()
+            elif command == "peers":
+                peers = get_peers(
+                    url=torrent.tracker_url,
+                    info=torrent.info,
+                    left=torrent.length,
+                )
+                for peer in peers:
+                    print(peer)
+            elif command == "handshake":
+                ip, port = sys.argv[3].split(":")
+                # print(f"Connecting to {ip}:{port}")
+                perform_handshake_standalone(ip, port, torrent.info_hash)
+        case "download_piece":
+            output_file_path = sys.argv[3]
+            torrent = Torrent.from_file(sys.argv[4])
+            total_number_of_pieces = len(torrent.pieces)
+            print(f"Total number of pieces: {total_number_of_pieces}")
+            torrent_file_content = read_torrent_file_raw(sys.argv[4])
+            piece_index = int(sys.argv[5])
             download_piece(
                 torrent_file_content,
                 piece_index,
                 output_file_path,
                 total_number_of_pieces,
             )
+        case "download":
+            output_file_path = sys.argv[3]
+            torrent = Torrent.from_file(sys.argv[4])
+            total_number_of_pieces = len(torrent.pieces)
+            print(f"Total number of pieces: {total_number_of_pieces}")
+            torrent_file_content = read_torrent_file_raw(sys.argv[4])
 
-        with open(output_file_path, "wb") as final_file:
             for piece_index in range(total_number_of_pieces):
-                piece_file_name = f"{output_file_path}.part{piece_index}"
-                with open(piece_file_name, "rb") as piece_file:
-                    final_file.write(piece_file.read())
-                os.remove(piece_file_name)
+                download_piece(
+                    torrent_file_content,
+                    piece_index,
+                    output_file_path,
+                    total_number_of_pieces,
+                )
 
-    else:
-        raise NotImplementedError(f"Unknown command {command}")
+            with open(output_file_path, "wb") as final_file:
+                for piece_index in range(total_number_of_pieces):
+                    piece_file_name = f"{output_file_path}.part{piece_index}"
+                    with open(piece_file_name, "rb") as piece_file:
+                        final_file.write(piece_file.read())
+                    os.remove(piece_file_name)
+        case _:
+            raise NotImplementedError(f"Unknown command {command}")
 
 
 if __name__ == "__main__":
